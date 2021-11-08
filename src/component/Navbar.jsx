83b6c8bdeee696d05ccbc2541d581698cfe1a4b2
Raviy Bayu S <bayu.setiaji709@gmail.com>
2021-11-08 16:47:06 +0700

    add-todo

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -13,7 +13,9 @@ const Navbar = ({author, app}) => {
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Navbar</a>
+                <a className="navbar-brand" href="/"><button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Todos from "../component/Todo/Todos";
 
 const Home = () => {
     return (
         <div>
             <div className="container py-3">
-                HOME
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae atque optio deleniti reiciendis culpa, error fugit quaerat fuga, commodi repudiandae ratione labore! Dignissimos suscipit minima nobis qui corporis deserunt hic!</p>
+                <div>
+                    <div className='container' style={{marginTop: "200px"}}>
+                        <div className='row d-flex justify-content-center align-items-center'>
+                            <div className='col-7'>
+                                <h1 className='text-center' style={{fontSize:"70px"}}>T o d o S</h1>
+                                <Todos />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
