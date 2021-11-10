5073c7caa1a65213b33e05b6a8f727419bb45c0f
Raviy Bayu S <bayu.setiaji709@gmail.com>
2021-11-10 11:29:07 +0700

    add-route

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import { PersistGate } from 'redux-persist/integration/react';
 import './App.css';
 import Home from './component/Home';
 import { store, persistor } from './store/store';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
+            <Router>
+              <Navbar />
+            </Router>
             <Home />
           </PersistGate>
         </Provider>
---
+++ src/components/SidebarData.js
@@ -0,0 +1,17 @@
+export const SidebarData = [
+    {
+        title: "Home",
+        path: "/",
+        cName: "nav-text",
+    },
+    {
+        title: "About App",
+        path: "/about/about-app",
+        cName: "nav-text",
+    },
+    {
+        title: "About Author",
+        path: "/about/about-author",
+        cName: "nav-text",
+    },
+];
